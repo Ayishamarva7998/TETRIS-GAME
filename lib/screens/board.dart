c8441a355f697b5a2ae233c6a0aed202df15d990
Ayishamarva7998 <ayshaaysha7998@gmail.com>
2024-06-12 15:57:15 +0530

    positions condittions added

--- lib/screens/board.dart
+++ lib/screens/board.dart
@@ -1,5 +1,9 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:tetris/screens/piece.dart';
 import 'package:tetris/screens/pixels.dart';
+import 'package:tetris/screens/values.dart';
 
 class GameBoard extends StatefulWidget {
   const GameBoard({super.key});
@@ -12,7 +16,18 @@ class _GameBoardState extends State<GameBoard> {
   //game dimensions
   int rowLength = 10;
   int colLength = 15;
+
+  //current tetris piece
+  piece currentPiece = piece(type: Tetris.L);
+
   @override
+  void initState() {
+    super.initState();
+    startGame();
+  }
+  void startGame(){
+    currentPiece.initializedPiece();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
@@ -21,9 +36,18 @@ class _GameBoardState extends State<GameBoard> {
         physics:const NeverScrollableScrollPhysics(),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: rowLength),
-          itemBuilder: (context, index) => Center(
-                child: Pixels(color: Colors.grey[900], child: index)
-              )),
+          itemBuilder: (context, index) {
+            // return Pixels(color: Colors.grey[900], child: index);
+            if(currentPiece.position.contains(index)){
+              return Pixels(color: Colors.yellow, child: index);
+              
+            }else{
+              return Pixels(color: Colors.grey[900], child: index) ;
+            }
+          }
+          // Pixels(color: Colors.grey[900], child: index)
+          
+          ),
     );
   }
 }
